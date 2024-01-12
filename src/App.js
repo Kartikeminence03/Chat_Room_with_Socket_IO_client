5dfa387f8de1713429762b213322b58f8af4d257
Kartikeminence03 <kartik.eminence@gmail.com>
2024-01-12 17:32:06 +0530

    This is chat room client site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import { io } from "socket.io-client";
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 function App() {
+  const socket = useMemo(
+    () =>
+      io("http://localhost:5000", {
+        withCredentials: true,
+      }),
+    []
+  );
+
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketID, setSocketId] = useState("");
+  const [roomName, setRoomName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { message, room });
+    setMessage("");
+  };
+
+  const joinRoomHandler = (e) => {
+    e.preventDefault();
+    socket.emit("join-room", roomName);
+    setRoomName("");
+  };
+
+  useEffect(()=>{
+    socket.on('connect',()=>{
+      setSocketId(socket.id);
+      console.log('connected', socket.id);
+    });
+
+    socket.on("receive-message", (data) => {
+      console.log(data);
+      setMessages((messages) => [...messages, data]);
+    });
+
+    return ()=>{
+      socket.disconnect();
+    }
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ height: 500 }} />
+      <Typography variant="h6" component="div" gutterBottom>
+        {socketID}
+      </Typography>
+
+      <form onSubmit={joinRoomHandler}>
+      <h5>Join Room</h5>
+        <TextField
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic"
+          label="Room Name"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+      </form>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          id="outlined-basic"
+          label="Message"
+          variant="outlined"
+        />
+        <TextField
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic"
+          label="Room"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+
+      <Stack>
+        {messages.map((m, i) => (
+            <Typography key={i} variant="h6" component="div" gutterBottom>
+              {m}
+            </Typography>
+          ))}
+      </Stack>
+    </Container>
   );
 }
 
